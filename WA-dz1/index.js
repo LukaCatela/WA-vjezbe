73abfdae9f686616843bcba0a9f7ec8ef522b962
Luka Catela <luca.catela8@gmail.com>
2025-10-29 17:01:43 +0100

    pola posla gotovo

--- WA-dz1/index.js
+++ WA-dz1/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-app.listen(PORT);
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// dodao sam path jer sendFile prima samo jedan argument, odnosno putanju do file-a
+
+app.get('/about',(req, res) => {
+    res.sendFile(__dirname + '/public/about.html')
+});
+
+// ovo je drugi nacin prikaza
+app.listen(PORT, error => {
+    if (error) {
+        console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+    } else {
+        console.log(`Server je pokrenut na http://localhost:${PORT}`);
+    }
+});
