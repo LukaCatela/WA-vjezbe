279b59f946c7be97f375ecb3c26cef9783841538
Luka Catela <luca.catela8@gmail.com>
2025-10-29 17:26:47 +0100

    Gotovi prvi samostalni zadatak

--- WA-dz1/index.js
+++ WA-dz1/index.js
@@ -3,6 +3,12 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
+const users = [
+        { id: 1, ime: 'Luka', prezime: 'Catela' },
+        { id: 2, ime: 'Ana', prezime: 'Kovač' },
+        { id: 3, ime: 'Marko', prezime: 'Marić' }
+    ];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -10,10 +16,14 @@ app.get('/', (req, res) => {
 // dodao sam path jer sendFile prima samo jedan argument, odnosno putanju do file-a
 
 app.get('/about',(req, res) => {
-    res.sendFile(__dirname + '/public/about.html')
+    res.sendFile(__dirname + '/public/about.html');
 });
 
 // ovo je drugi nacin prikaza
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 app.listen(PORT, error => {
     if (error) {
         console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
